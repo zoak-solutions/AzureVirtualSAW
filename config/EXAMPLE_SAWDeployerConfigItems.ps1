72394367f0c61c9b4398d25dd4362e5c1622bbdb
Mark Culhane <mark.culhane@zoak.solutions>
2023-11-08 23:27:12 +1100

    updating config format

--- config/EXAMPLE_SAWDeployerConfigItems.ps1
+++ config/EXAMPLE_SAWDeployerConfigItems.ps1
@@ -31,38 +31,42 @@ $AzureFWRange = "10.0.99.0/26"
 $AzureFWAllocationMethod = 'Static'
 $AzureFWSKU = "Standard"
 $AzureFWPublicIPName = "$AzureFWName-PubIP"
+$SAWFWPolicyName = "SAWFWPolicy"
 $SAWFWNetRuleCollName = "SAWFWNetColl01"
 $SAWFWAppRuleCollName = "SAWFWAppColl01"
 ######################################################
 # SAW HTTP/S outbound Allowed hosts for SAWs as hashtable, no s and a key that meaningfully describes the host
+
+
 $SAWFWAppRules = @(
-    '-Name "windows-update-rule" -FqdnTag WindowsUpdate -SourceAddress "' + $SAWSubNetRange + '"',
-    '-Name "AzurVDITagAllow" -FqdnTag @("WindowsVirtualDesktop", "AzureFrontDoor.Frontend", "AzureMonitor") -SourceAddress "' + $SAWSubNetRange + '" -Protocol http, https',
-    '-Name "google" -SourceAddress "' + $SAWSubNetRange + '" -Protocol http, https -TargetFqdn "google.com"',
-    '-Name "googleapis" -SourceAddress "' + $SAWSubNetRange + '" -Protocol http, https -TargetFqdn "googleapis.com"',
-    '-Name "microsoft" -SourceAddress "' + $SAWSubNetRange + '" -Protocol http, https -TargetFqdn "microsoft.com"',
-    '-Name "microsoft_online" -SourceAddress "' + $SAWSubNetRange + '" -Protocol http, https -TargetFqdn "microsoftonline.com"',
-    '-Name "msftconnecttest" -SourceAddress "' + $SAWSubNetRange + '" -Protocol http, https -TargetFqdnmsftconnecttest.com"',
-    '-Name "MSTLD" -SourceAddress "' + $SAWSubNetRange + '" -Protocol http, https -TargetFqdn "ms"',
-    '-Name "azure" -SourceAddress "' + $SAWSubNetRange + '" -Protocol http, https -TargetFqdn "azure.com"',
-    '-Name "AzureDNS" -SourceAddress "' + $SAWSubNetRange + '" -Protocol http, https -TargetFqdn "azure-dns.net"',
-    '-Name "azureEdge" -SourceAddress "' + $SAWSubNetRange + '" -Protocol http, https -TargetFqdnazureedge.net"',
-    '-Name "azureEndpoints" -SourceAddress "' + $SAWSubNetRange + '" -Protocol http, https -TargetFqdn "azure.net"',
-    '-Name "windows" -SourceAddress "' + $SAWSubNetRange + '" -Protocol http, https -TargetFqd nwindows.net"',
-    '-Name "1password" -SourceAddress "' + $SAWSubNetRange + '" -Protocol http, https -TargetFqdn "1password.com'   
-    '-Name "apple" -SourceAddress "' + $SAWSubNetRange + '" -Protocol http, https -TargetFqdn "apple.com"',
-    '-Name "atlassian" -SourceAddress "' + $SAWSubNetRange + '" -Protocol http, https -TargetFqdn "atlassian.com"',
-    '-Name "bitbucket" -SourceAddress "' + $SAWSubNetRange + '" -Protocol http, https -TargetFqdn "bitbucket.org"',
-    '-Name "digicert" -SourceAddress "' + $SAWSubNetRange + '" -Protocol http, https -TargetFqdn "digicert.com"',
-    '-Name "github" -SourceAddress "' + $SAWSubNetRange + '" -Protocol http, https -TargetFqdn "github.com"'
+    @{Name = "windows-update-rule"; FqdnTag = "WindowsUpdate"; SourceAddress = $SAWSubNetRange }
+    @{Name = "AzurVDITagAllow"; FqdnTag = @("WindowsVirtualDesktop", "AzureFrontDoor.Frontend", "AzureMonitor"); SourceAddress = $SAWSubNetRange; Protocol = @("http", "https") }
+    @{Name = "google"; SourceAddress = $SAWSubNetRange; Protocol = @("http", "https"); TargetFqdn = "google.com" }
+    @{Name = "googleapis"; SourceAddress = $SAWSubNetRange; Protocol = @("http", "https"); TargetFqdn = "googleapis.com" }
+    @{Name = "microsoft"; SourceAddress = $SAWSubNetRange; Protocol = @("http", "https"); TargetFqdn = "microsoft.com" }
+    @{Name = "microsoft_online"; SourceAddress = $SAWSubNetRange; Protocol = @("http", "https"); TargetFqdn = "microsoftonline.com" }
+    @{Name = "msftconnecttest"; SourceAddress = $SAWSubNetRange; Protocol = @("http", "https"); TargetFqdn = "msftconnecttest.com" }
+    @{Name = "MSTLD"; SourceAddress = $SAWSubNetRange; Protocol = @("http", "https"); TargetFqdn = "ms" }
+    @{Name = "azure"; SourceAddress = $SAWSubNetRange; Protocol = @("http", "https"); TargetFqdn = "azure.com" }
+    @{Name = "AzureDNS"; SourceAddress = $SAWSubNetRange; Protocol = @("http", "https"); TargetFqdn = "azure-dns.net" }
+    @{Name = "azureEdge"; SourceAddress = $SAWSubNetRange; Protocol = @("http", "https"); TargetFqdn = "azureedge.net" }
+    @{Name = "azureEndpoints"; SourceAddress = $SAWSubNetRange; Protocol = @("http", "https"); TargetFqdn = "azure.net" }
+    @{Name = "windows"; SourceAddress = $SAWSubNetRange; Protocol = @("http", "https"); TargetFqdn = "windows.net" }
+    @{Name = "1password"; SourceAddress = $SAWSubNetRange; Protocol = @("http", "https"); TargetFqdn = "1password.com" }
+    @{Name = "apple"; SourceAddress = $SAWSubNetRange; Protocol = @("http", "https"); TargetFqdn = "apple.com" }
+    @{Name = "atlassian"; SourceAddress = $SAWSubNetRange; Protocol = @("http", "https"); TargetFqdn = "atlassian.com" }
+    @{Name = "bitbucket"; SourceAddress = $SAWSubNetRange; Protocol = @("http", "https"); TargetFqdn = "bitbucket.org" }
+    @{Name = "digicert"; SourceAddress = $SAWSubNetRange; Protocol = @("http", "https"); TargetFqdn = "digicert.com" }
+    @{Name = "github"; SourceAddress = $SAWSubNetRange; Protocol = @("http", "https"); TargetFqdn = "github.com" }
 )
+
 # As per: https://learn.microsoft.com/en-us/azure/firewall/protect-azure-virtual-desktop?tabs=azure#create-network-rules
 $SAWFWNetRules = @(
-    '-Name "Azure Instance Metadata service" -SourceAddress "' + $SAWSubNetRange + '" -Protocol TCP -DestinationAddress @("169.254.169.254/32") -DestinationPort @("80", "443")',
-    '-Name "Session host monitoring" -SourceAddress ' + $SAWSubNetRange + '" -Protocol TCP -DestinationAddress @("168.63.129.16/32") -DestinationPort @("80", "443")',
-    '-Name "DNS to AzureFW" -SourceAddress ' + $SAWSubNetRange + '" -Protocol TCP,UDP -DestinationAddress "' + $AzureFWRange + '" -DestinationPort @("53")',
-    '-Name "azkms.core.windows.net IPs" -SourceAddress ' + $SAWSubNetRange + '" -Protocol TCP -DestinationAddress @("20.118.99.224","40.83.235.53","23.102.135.246") -DestinationPort @("1688")',
-    '-Name "Session host monitoring" -SourceAddress ' + $SAWSubNetRange + '" -Protocol TCP -DestinationAddress @("168.63.129.16/32") -DestinationPort @("80", "443")',
+    @{Name = "Azure Instance Metadata service"; SourceAddress = $SAWSubNetRange; Protocol = "TCP"; DestinationAddress = @("169.254.169.254/32"); DestinationPort = @("80", "443") }
+    @{Name = "Session host monitoring"; SourceAddress = $SAWSubNetRange; Protocol = "TCP"; DestinationAddress = @("168.63.129.16/32"); DestinationPort = @("80", "443") }
+    @{Name = "DNS to AzureFW"; SourceAddress = $SAWSubNetRange; Protocol = @("TCP", "UDP"); DestinationAddress = $AzureFWRange; DestinationPort = @("53") }
+    @{Name = "azkms.core.windows.net IPs"; SourceAddress = $SAWSubNetRange; Protocol = "TCP"; DestinationAddress = @("20.118.99.224", "40.83.235.53", "23.102.135.246"); DestinationPort = @("1688") }
+    @{Name = "Session host monitoring"; SourceAddress = $SAWSubNetRange; Protocol = "TCP"; DestinationAddress = @("168.63.129.16/32"); DestinationPort = @("80", "443") }
 )
 
 # Array of SAW users to add to the SAW User Group
